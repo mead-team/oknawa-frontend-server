7a1f547b447c55347ab282286a2ed11eff864d99
SeungJu <asjj8051@gmail.com>
2023-11-19 14:58:31 +0900

    feat: PWA 기초 세팅

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -15,6 +15,7 @@ export default function BaseHead() {
       <meta name="description" content="Ok, 나와!" />
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="manifest" href="/manifest.json" />
     </head>
   );
 }
