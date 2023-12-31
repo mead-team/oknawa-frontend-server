bcb12a52177a85109e5cb437435292f5c582d1ee
BaeJungKyu <junggyoo.1@gmail.com>
2023-12-31 17:46:18 +0900

    faet: 카카오맵 bounds 추가

--- src/views/result/components/ResultMap.tsx
+++ src/views/result/components/ResultMap.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useAtom, useAtomValue } from 'jotai';
 import styled from 'styled-components';
 import {
@@ -10,6 +12,7 @@ import {
 import { searchState } from '@/jotai/global/store';
 import { resultState } from '@/jotai/result/store';
 import CenterMarker from './CenterMarker';
+import { useEffect, useMemo, useRef } from 'react';
 
 const getStrokeColor = (index: number) => {
   switch (index) {
@@ -27,6 +30,8 @@ const getStrokeColor = (index: number) => {
 };
 
 export default function ResultMap() {
+  const mapRef = useRef<any>(null);
+
   const [result] = useAtom(resultState);
   const searchValue = useAtomValue(searchState);
 
@@ -36,38 +41,59 @@ export default function ResultMap() {
 
   const stationName = result.station_name.split(' ')[0];
 
+  const bounds = useMemo(() => {
+    const bounds = new kakao.maps.LatLngBounds();
+    searchValue?.forEach(user => {
+      const { latitude, longitude } = user.address;
+      const position = new kakao.maps.LatLng(latitude, longitude);
+      bounds.extend(position);
+    });
+    return bounds;
+  }, [searchValue]);
+
+  useEffect(() => {
+    const map = mapRef.current;
+    map?.setBounds(bounds);
+  }, [bounds]);
+
   return (
-    <MapCenter center={{ lat: result.end_y, lng: result.end_x }} level={3}>
-      {searchValue?.map((user, index) => {
-        return (
-          <MapMarker
-            key={index}
-            position={{
-              lat: user.address.latitude,
-              lng: user.address.longitude,
-            }}
-            image={{
-              src: `/images/marker${index}.svg`,
-              size: { width: 30, height: 39 },
-            }}
-          />
-        );
-      })}
-      <CustomOverlayMap position={{ lat: result.end_y, lng: result.end_x }}>
-        <CenterMarker>{stationName}</CenterMarker>
-      </CustomOverlayMap>
-      {polylines.map((polyline, index) => {
-        return (
-          <Polyline
-            key={index}
-            path={polyline}
-            strokeWeight={4}
-            strokeOpacity={1}
-            strokeColor="#18C964"
-          />
-        );
-      })}
-    </MapCenter>
+    <>
+      <MapCenter
+        center={{ lat: result.end_y, lng: result.end_x }}
+        level={3}
+        ref={mapRef}
+      >
+        {searchValue?.map((user, index) => {
+          return (
+            <MapMarker
+              key={index}
+              position={{
+                lat: user.address.latitude,
+                lng: user.address.longitude,
+              }}
+              image={{
+                src: `/images/marker${index}.svg`,
+                size: { width: 30, height: 39 },
+              }}
+            />
+          );
+        })}
+        <CustomOverlayMap position={{ lat: result.end_y, lng: result.end_x }}>
+          <CenterMarker>{stationName}</CenterMarker>
+        </CustomOverlayMap>
+        {polylines.map((polyline, index) => {
+          return (
+            <Polyline
+              key={index}
+              path={polyline}
+              strokeWeight={5}
+              strokeOpacity={1}
+              strokeColor="#18C964"
+            />
+          );
+        })}
+      </MapCenter>
+    </>
   );
 }
 
