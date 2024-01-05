8130ef4b51a8f8d93cd9e8e640758ba50d36cd57
BaeJungKyu <junggyoo.1@gmail.com>
2024-01-05 12:20:42 +0900

    feat: RootLayout에 metadata 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,27 @@ import { Providers } from './providers';
 
 import StyledComponentsRegistry from '@/styles/registry';
 import '@/styles/globals.css';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Ok, Nawa!',
+  description: '만나기 편한 중간 장소와 핫플레이스를 추천해드려요!',
+  keywords:
+    'oknawa, 오케이나와, 오키나와, 약속 장소 추천, 핫플레이스, 중간 장소 추천, ok nawa, oknawa.com',
+  openGraph: {
+    url: 'https://oknawa.com',
+    title: 'Ok, Nawa!',
+    description: '만나기 편한 중간 장소와 핫플레이스를 추천해드려요!',
+    images: [
+      {
+        url: 'https://oknawa.com/icons/icon-192x192.png',
+        width: 129,
+        height: 129,
+        alt: 'Ok, Nawa!',
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
