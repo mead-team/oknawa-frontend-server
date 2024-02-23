138ef2d0b72331d2001093728edfc4d91016f500
lbo728 <72309817+lbo728@users.noreply.github.com>
2024-02-23 17:09:53 +0900

    feat: useSearchForm -> userSection defaultValues에 searchList 데이터 케이스 추가

--- src/hooks/form/search/useSearchForm.tsx
+++ src/hooks/form/search/useSearchForm.tsx
@@ -1,4 +1,6 @@
+import { searchState } from '@/jotai/global/store';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { useAtom } from 'jotai';
 import { useForm } from 'react-hook-form';
 import { array, number, object, string } from 'yup';
 
@@ -19,18 +21,32 @@ const createSearchFormVerifySchema = () => {
 };
 
 export default function useSearchForm() {
+  const [searchList] = useAtom(searchState);
+
   return useForm({
     defaultValues: {
-      userSection: [
-        {
-          name: '',
-          address: { fullAddress: '', latitude: 0, longitude: 0 },
-        },
-        {
-          name: '',
-          address: { fullAddress: '', latitude: 0, longitude: 0 },
-        },
-      ],
+      userSection:
+        searchList.length < 2
+          ? [
+              {
+                name: '',
+                address: { fullAddress: '', latitude: 0, longitude: 0 },
+              },
+              {
+                name: '',
+                address: { fullAddress: '', latitude: 0, longitude: 0 },
+              },
+            ]
+          : searchList.map((serach, index) => {
+              return {
+                name: `${searchList[index]?.name}`,
+                address: {
+                  fullAddress: `${searchList[index]?.address?.fullAddress}`,
+                  latitude: searchList[index]?.address?.latitude,
+                  longitude: searchList[index]?.address?.longitude,
+                },
+              };
+            }),
     },
     resolver: yupResolver(createSearchFormVerifySchema()),
   });
