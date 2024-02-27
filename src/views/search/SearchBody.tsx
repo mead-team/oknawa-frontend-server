742a181553ac99609362e6f3037785c7ced209a5
lbo728 <72309817+lbo728@users.noreply.github.com>
2024-02-27 22:37:56 +0900

    fix: SearchBody -> console.log 제거

--- src/views/search/SearchBody.tsx
+++ src/views/search/SearchBody.tsx
@@ -80,7 +80,6 @@ export default function SearchBody() {
         setResult(data);
       },
     });
-    console.log('searchForm??', searchForm);
   };
 
   const addressValue = watch('userSection');
